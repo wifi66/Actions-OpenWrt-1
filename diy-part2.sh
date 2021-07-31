8e08a6bb877ec83e1b6159db36a82771f42ddfc9
rq1025330 <462290029@qq.com>
2021-07-31 22:18:22 +0800

    update

--- diy-part2.sh
+++ diy-part2.sh
@@ -32,7 +32,7 @@ svn co https://github.com/lisaac/luci-app-dockerman/trunk/applications/luci-app-
 git clone https://github.com/rufengsuixing/luci-app-autoipsetadder.git package/luci-app-autoipsetadder
 git clone https://github.com/mchome/openwrt-dogcom.git package/openwrt-dogcom
 git clone https://github.com/mchome/luci-app-dogcom.git package/luci-app-dogcom
-#git clone https://github.com/kiddin9/luci-app-dnsfilter package/luci-app-dnsfilter
+#git clone https://github.com/garypang13/luci-app-dnsfilter package/luci-app-dnsfilter
 git clone https://github.com/small-5/luci-app-adblock-plus package/luci-app-adblock-plus
 git clone https://github.com/project-lede/luci-app-godproxy package/luci-app-godproxy
 
@@ -127,16 +127,16 @@ git clone https://github.com/sirpdboy/luci-app-advanced package/luci-app-advance
 svn co https://github.com/ophub/luci-app-amlogic/trunk/luci-app-amlogic package/luci-app-amlogic
 #修改晶晨宝盒默认配置
 # 1.Set the download repository of the OpenWrt files to your github.com （OpenWrt 文件的下载仓库）
-sed -i "s|ophub/amlogic-s9xxx-openwrt|HoldOnBro/Actions-OpenWrt|g" package/luci-app-amlogic/root/etc/config/amlogic
+sed -i "s|https.*/amlogic-s9xxx-openwrt|https://github.com/HoldOnBro/Actions-OpenWrt|g" package/luci-app-amlogic/root/etc/config/amlogic
 
-# 2.Set the download path of the kernel in your github.com repository （OpenWrt 内核的下载路径）
-sed -i "s|amlogic-s9xxx/amlogic-kernel|BuildARMv8|g" package/luci-app-amlogic/root/etc/config/amlogic
-
-# 3.Modify the keywords of Tags in your github.com Releases （Releases 里 Tags 的关键字）
+# 2.Modify the keywords of Tags in your github.com Releases （Releases 里 Tags 的关键字）
 sed -i "s|s9xxx_lede|ARMv8|g" package/luci-app-amlogic/root/etc/config/amlogic
 
-# 4.Modify the suffix of the OPENWRT files in your github.com Releases （Releases 里 OpenWrt 文件的后缀）
+# 3.Modify the suffix of the OPENWRT files in your github.com Releases （Releases 里 OpenWrt 文件的后缀）
 sed -i "s|.img.gz|+_FOL+SFE.img.gz|g" package/luci-app-amlogic/root/etc/config/amlogic
+
+# 4.Set the download path of the kernel in your github.com repository （OpenWrt 内核的下载路径）
+sed -i "s|http.*/library|https://github.com/HoldOnBro/Actions-OpenWrt/tree/master/BuildARMv8|g" package/luci-app-amlogic/root/etc/config/amlogic
 
 #添加argon-config 使用 最新argon
 git clone https://github.com/jerrykuku/luci-app-argon-config package/luci-app-argon-config
--- diy-part3.sh
+++ diy-part3.sh
@@ -56,14 +56,14 @@ svn co https://github.com/kenzok8/openwrt-packages/trunk/smartdns package/smartd
 
 svn co https://github.com/ophub/luci-app-amlogic/trunk/luci-app-amlogic package/luci-app-amlogic
 # Modify the default configuration of Amlogic Box
-#1.Set the download repository of the OpenWrt files to your github.com （OpenWrt 文件的下载仓库）
-sed -i "s|ophub/amlogic-s9xxx-openwrt|rq1025330/Actions-OpenWrt|g" package/luci-app-amlogic/root/etc/config/amlogic
-#2.Set the download path of the kernel in your github.com repository （OpenWrt 内核的下载路径）
-sed -i "s|amlogic-s9xxx/amlogic-kernel|BuildARMv8|g" package/luci-app-amlogic/root/etc/config/amlogic
-#3.Modify the keywords of Tags in your github.com Releases （Releases 里 Tags 的关键字）
+# 1.Set the download repository of the OpenWrt files to your github.com （OpenWrt 文件的下载仓库）
+sed -i "s|https.*/amlogic-s9xxx-openwrt|https://github.com/rq1025330/Actions-OpenWrt|g" package/luci-app-amlogic/root/etc/config/amlogic
+# 2.Set the keywords of Tags in your github.com Releases （Releases 里 Tags 的关键字）
 sed -i "s|s9xxx_lede|ARMv8|g" package/luci-app-amlogic/root/etc/config/amlogic
-#4.Modify the suffix of the OPENWRT files in your github.com Releases （Releases 里 OpenWrt 文件的后缀）
+# 3.Set the suffix of the OPENWRT files in your github.com Releases （Releases 里 OpenWrt 文件的后缀）
 sed -i "s|.img.gz|MY.img.gz|g" package/luci-app-amlogic/root/etc/config/amlogic
+# 4.Set the download path of the kernel in your github.com repository （OpenWrt 内核的下载路径）
+sed -i "s|https.*/library|https://github.com/rq1025330/Actions-OpenWrt/tree/master/BuildARMv8|g" package/luci-app-amlogic/root/etc/config/amlogic
 
 svn co https://github.com/vernesong/OpenClash/trunk/luci-app-openclash package/luci-app-openclash
 # 编译 po2lmo (如果有po2lmo可跳过)
